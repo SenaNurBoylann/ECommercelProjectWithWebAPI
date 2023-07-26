0546ff87bffdd679517e128a978e6677f4b167ba
SenaNurBoylann <senaboylan@marun.edu.tr>
2023-07-26 10:28:25 +0300

    Add project files.

---
+++ WebAPICoreWithMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAPICoreWithMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
