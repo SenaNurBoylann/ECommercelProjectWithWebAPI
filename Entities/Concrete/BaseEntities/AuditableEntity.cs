0d7dc0d91bc51045048d2de02b1c6160f66c8ac3
SenaNurBoylann <senaboylan@marun.edu.tr>
2023-07-26 11:44:06 +0300

    Entities Concete de AuditableEntity oluşturdum.

---
+++ Entities/Concrete/BaseEntities/AuditableEntity.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete.BaseEntities
+{
+    public class AuditableEntity : BaseEntity, ICreatedEntity, IUpdatedEntity
+    {
+        public int CreatedUserId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime CreatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int? UpdateUserId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime? UpdatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
+                                                                        
